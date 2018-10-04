30979fc5faee30d07f62cce03884ee4b3f73deb0
jheske <jeheske@gmail.com>
2018-10-04 10:40:21 -0400

    Add a ViewModel to manage team scores

---
+++ app/src/main/java/com/example/android/courtcounter/TeamScoresViewModel.java
@@ -0,0 +1,14 @@
+package com.example.android.courtcounter;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by jill on {DATE}
+ */
+public class TeamScoresViewModel extends ViewModel {
+
+    // Create Model variables to hold team scores
+    int scoreTeamA;
+    int scoreTeamB;
+
+}
