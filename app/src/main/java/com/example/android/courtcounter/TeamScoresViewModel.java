3feda75eb63d2037e7abb1f6d05fb558d9a8f257
jheske <jeheske@gmail.com>
2018-10-04 14:16:07 -0400

    Update comments

--- app/src/main/java/com/example/android/courtcounter/TeamScoresViewModel.java
+++ app/src/main/java/com/example/android/courtcounter/TeamScoresViewModel.java
@@ -4,7 +4,7 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
