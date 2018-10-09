c0747a9b0adfb9d0bf2f18da1cf57317fc06ee06
jheske <jeheske@gmail.com>
2018-10-09 12:15:04 -0400

    Add TextViews for counters

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -119,15 +119,27 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreView =  findViewById(R.id.team_a_score);
+        TextView onePointView =  findViewById(R.id.team_a_one_point);
+        TextView twoPointView =  findViewById(R.id.team_a_two_point);
+        TextView threePointView = findViewById(R.id.team_a_three_point);
+        threePointView.setText("+3 Points " + String.valueOf(onePointCountTeamA) + ")");
+        twoPointView.setText("+2 Points " + String.valueOf(twoPointCountTeamA) + ")");
+        onePointView.setText("Free Throw " + String.valueOf(threePointCountTeamA) + ")");
+        scoreView.setText("SCORE " + String.valueOf(score));
     }
 
     /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreView =  findViewById(R.id.team_b_score);
+        TextView onePointView =  findViewById(R.id.team_b_one_point);
+        TextView twoPointView = findViewById(R.id.team_b_two_point);
+        TextView threePointView = findViewById(R.id.team_b_three_point);
+        threePointView.setText("+3 Points (" + String.valueOf(onePointCountTeamB) + ")");
+        twoPointView.setText("+2 Points (" + String.valueOf(twoPointCountTeamB) + ")");
+        onePointView.setText("Free Throw (" + String.valueOf(threePointCountTeamB) + ")");
+        scoreView.setText("SCORE " + String.valueOf(score));
     }
 }
